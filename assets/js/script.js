54ea97f3a7cac6e52422935d053e8b281907d38c
David Silva <contatodavidmaia@gmail.com>
2022-07-31 14:56:07 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const horas = document.querySelector('#horas');
+const minutos = document.querySelector('#minutos');
+const segundos = document.querySelector('#segundos');
+const partDay = document.querySelector('#am-pm');
+
+const relogio = setInterval(function time() {
+    let data = new Date();
+    let hour = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+
+    if (hour < 10) hr = '0' + hour;
+    if (min < 10) min = '0' + min;
+    if (seg < 10) seg = '0' + seg;
+
+    if (hour < 12) {
+        partDay.textContent = 'AM'
+    } else {
+        partDay.textContent = 'PM'
+    }
+
+    horas.textContent = hour;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+})
